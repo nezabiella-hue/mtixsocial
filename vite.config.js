2c139959655c5acfe49db2594a21043e39c1e1a8
nezabiella-hue <nezabiella@gmail.com>
2026-01-11 03:48:02 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
+  plugins: [tailwindcss()],
 });
